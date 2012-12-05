d0ec2b0225ea26565cf43e647afb03e1c371d034
Marak Squires <marak.squires@gmail.com>
2012-12-05 11:16:31 +0530

    [fix] Undo __dirname resource loading ( for now )

--- index.js
+++ index.js
@@ -113,71 +113,63 @@ resource.load = function (r) {
   //
   // TODO: Check DIRNAME before checking process.cwd()
   //
+
+  //
+  // Check to see if the resource exists in the $CWD/resources/ path
+  //
   try {
-    require.resolve(__dirname + '/resources/' + r)
+    require.resolve(process.cwd() + '/resources/' + r)
     //
     // If so, require it
     //
-    result = require(__dirname + '/resources/' + r);
+    result = require(process.cwd() + '/resources/' + r);
   } catch (err) {
+    //
+    // If not, check to see if the resource exists in "resources" package on npm
+    //
 
     //
-    // Check to see if the resource exists in the $CWD/resources/ path
+    // Attempt to resolve "resources"
     //
+    var p = require.resolve('resources');
+    p = p.replace('/index.js', '/');
+    p += r;
     try {
-      require.resolve(process.cwd() + '/resources/' + r)
-      //
-      // If so, require it
-      //
-      result = require(process.cwd() + '/resources/' + r);
+      require.resolve(p);
     } catch (err) {
       //
-      // If not, check to see if the resource exists in "resources" package on npm
+      // Resource was not found in $CWD/resources/ or in the "resources" npm package
       //
-
-      //
-      // Attempt to resolve "resources"
-      //
-      var p = require.resolve('resources');
-      p = p.replace('/index.js', '/');
-      p += r;
-      try {
-        require.resolve(p);
-      } catch (err) {
-        //
-        // Resource was not found in $CWD/resources/ or in the "resources" npm package
-        //
-        throw new Error('invalid resource' + p);
-      }
-
-      //
-      // Since the resource was found in the "resources" package, copy it to $CWD/resources
-      //
-      try {
-        require('fs').mkdirSync('./resources/');
-        // TODO: add more content to this README file
-        require('fs').writeFileSync('./resources/README.md', '# Resources Readme');
-      } catch (err) {
-        // do nothing
-      }
-
-      try {
-        require('fs').mkdirSync('./resources/'+ r);
-      } catch (err) {
-        // do nothing
-      }
-      logger.info('installing ' + r.magenta + ' to ' + (process.cwd() + '/' + r).grey);
-
-      //
-      // Perform a sync directory copy from node_modules folder to CWD
-      //
-      helper.copyDir(p, process.cwd() + '/resources/' + r);
-
-      //
-      // Copy the contents of  /resources/theresource/ to $CWD/resources/theresource
-      //
-      result = require(p);
+      throw new Error('invalid resource' + p);
     }
+
+    //
+    // Since the resource was found in the "resources" package, copy it to $CWD/resources
+    //
+    try {
+      require('fs').mkdirSync('./resources/');
+      // TODO: add more content to this README file
+      require('fs').writeFileSync('./resources/README.md', '# Resources Readme');
+    } catch (err) {
+      // do nothing
+    }
+
+    try {
+      require('fs').mkdirSync('./resources/'+ r);
+    } catch (err) {
+      // do nothing
+    }
+    logger.info('installing ' + r.magenta + ' to ' + (process.cwd() + '/' + r).grey);
+
+    //
+    // Perform a sync directory copy from node_modules folder to CWD
+    //
+    helper.copyDir(p, process.cwd() + '/resources/' + r);
+
+    //
+    // Copy the contents of  /resources/theresource/ to $CWD/resources/theresource
+    //
+    result = require(p);
 
   }
 
