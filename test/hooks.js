5a377bc98e68f14e84bacfbaed96d00d536ee11e
Joshua Holbrook <josh.holbrook@gmail.com>
2012-12-28 21:06:05 +0530

    [test] Failing test for setting after hooks before method definition

--- test/hooks.js
+++ test/hooks.js
@@ -116,13 +116,24 @@ test("remove beforeAll hooks on resource - run creature.create", function (t) {
   });
 });
 
-test("adding creature.before('poke') - before creature.poke is defined", function (t) {
+test("adding creature.before('poke') and creature.after('poke') - before creature.poke is defined", function (t) {
   creature.before('poke', function (data, next) {
-    data = "poked!";
+    data = "poked";
+    return next(null, data);
+  });
+  creature.after('poke', function (data, next) {
+    data += '!';
     return next(null, data);
   });
   t.equal(creature.methods.poke.before.length, 1, 'before hook for "poke" still defined');
-  t.end();
+  creature.method('poke', function(data, callback){
+    callback(null, data);
+  });
+  creature.poke('poked!', function (err, data) {
+    t.ok(!err, 'poked! - no error');
+    t.equal('poked!', data, 'poked! - result is "poked!"');
+    t.end();
+  });
 });
 
 test("defining and running creature.poke", function (t) {
@@ -186,29 +197,6 @@ test("define vehicle resource - with datasource config - and before and after ho
   });
 });
 
-test("define vehicle resource - with datasource config - and before and after hooks added on same tick", function (t) {
-  var vehicle = resource.define('vehicle', { config: { datasource: 'memory' }});
-  vehicle.property('fuel', {
-    "type": "number",
-    "default": 0
-  });
-  vehicle.before('create', function (data, next) {
-    data.id = "#99";
-    next(null, data);
-  });
-  vehicle.after('create', function (data, next) {
-    data.fuel = 100;
-    next(null, data);
-  });
-  t.equal(vehicle.create.before.length, 1);
-  vehicle.create({ id: '#88' }, function(err, result){
-    t.type(err, "null", 'vehicle create hooks applied - no error');
-    t.equal(result.id, '#99', 'vehicle.before("create") applied - result.id == "#99"');
-    t.equal(result.fuel, 100, 'vehicle.after("create") applied - result.fuel == 100');
-    t.end();
-  });
-});
-
 test("define clock resource - with datasource config - and asynchronous before hooks - called twice asynchronously", function (t) {
   var clock = resource.define('clock', { config: { datasource: 'memory' }});
 
@@ -244,36 +232,4 @@ test("define clock resource - with datasource config - and asynchronous before h
       t.similar(clicks.bar, ['tick', 'tock' ], 'clock.before("create") hooks applied - clicks.bar is ["tick", "tock"]');
     });
   }, 250);
-});
-
-test("adding another creature.after('create')", function (t) {
-  t.equal(creature.create.after.length, 2);
-
-  creature.after('create', function (data, next) {
-    data.life = 999;
-    next(null, data);
-  });
-
-  t.equal(creature.create.after.length, 3);
-
-  creature.after('create', function (data, next) {
-    //
-    // Remark: To modify .id requires creating a new object
-    // TODO: look into JugglingDB adapter to determine why
-    //
-    var obj = {};
-    Object.keys(data).forEach(function (prop) {
-      obj[prop] = data[prop];
-    });
-    obj.id = "bobby";
-    next(null, obj);
-  });
-
-  creature.create({ id: 'jimmy', life: 1 }, function (err, data){
-    t.equal(data.id, 'bobby', 'added second after hook - data.id == "bobby"');
-    t.equal(data.life, 999, 'data.life == 999');
-
-    t.type(err, "null", 'callback fired - no error');
-    t.end();
-  });
 });
