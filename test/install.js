724dfa8fa35e297f3ebee177bf859eca0c275bbb
Michael Williams <dinosaur@riseup.net>
2013-07-12 13:57:42 -0700

    [test] added test that fails at installing dependencies

---
+++ test/install.js
@@ -0,0 +1,44 @@
+var tap = require("tap"),
+    test = tap.test,
+    plan = tap.plan,
+    faker,
+    resource;
+
+test("load resource module", function (t) {
+  resource = require('../');
+  t.ok(resource, "object loaded");
+  t.end();
+});
+
+test("define faker resource", function (t) {
+  faker = resource.define('faker');
+  faker.dependencies = {
+    'Faker': '*'
+  };
+  t.ok(faker, "faker resource defined");
+  t.ok(faker.dependencies, "faker dependencies defined");
+  t.end();
+});
+
+test("define Faker.fullName method", function (t) {
+  var fullName = function (callback) {
+    var Faker = require('Faker'),
+        ret = Faker.Name.findName();
+    if (typeof callback === 'function') {
+      return callback(null, ret);
+    } else {
+      return ret;
+    }
+  };
+  faker.method('fullName', fullName);
+  t.ok(faker.fullName, "faker.color method defined");
+  t.end();
+});
+
+test("call faker.fullName method", function (t) {
+  faker.fullName(function(err, str) {
+    t.ok(!err, "no error");
+    t.ok(str, "faker.fullName returned");
+    t.end();
+  });
+});
